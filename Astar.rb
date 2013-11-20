9696741e6d3f780e7f486469e77ed2a33b3334e2
zainb <zainb.sallahuddin@7vals.com>
2013-11-20 16:23:49 +0500

    First commit

--- Astar.rb
+++ Astar.rb
@@ -11,40 +11,40 @@ class Astar
 		@width = width
 		@height = height
 		@array = Array.new(@width){Array.new(@height) {0}}
-		@openList = Hash.new 
-		@closedList = Hash.new
+		@openList = {}
+		@closedList = {}
 	
 	end			
 	
-	def find_path(st, ed, wall) #what is st or end
-		s_i = st[0]
-		s_j = st[1]
+	def find_path(index_of_start, index_of_end, wall) #what is st or end
+		start_i = index_of_start[0]
+		start_j = index_of_start[1]
 
-		e_i = ed[0]
-		e_j = ed[1]
+		end_i = index_of_end[0]
+		end_j = index_of_end[1]
                 
 		wall.each_index do|i|
 			#puts i
 			@array[wall[i][0]][wall[i][1]]="w"
 		end
 		
-		@array[s_i][s_j]="s"
-		@array[e_i][e_j]="e"
+		@array[start_i][start_j]="s"
+		@array[end_i][end_j]="e"
 
 		# Start of Algorithm
 		puts "Your Grid is:"
 		print " #{@array} \n "
-		@openList[[s_i,s_j]] = [0, 0, 0, nil, nil]
-		extractList(s_i, s_j, e_i, e_j)
+		@openList[[start_i,start_j]] = [0, 0, 0, nil, nil]
+		extract_list(start_i, start_j, end_i, end_j)
 		list = []
-		extractPath( e_i,e_j, s_i, s_j,list)
-		print [e_i, e_j] << list #would this be correct or should it be list=extractPath 
+		extract_path( end_i,end_j, start_i, start_j,list)
+		print [end_i, end_j] << list #would this be correct or should it be list=extractPath 
 	end
 	
 
 
-	def extractList(current_i, current_j, e_i,e_j)
-		until @closedList.include?([e_i, e_j]) || @openList.empty?
+	def extract_list(current_i, current_j, end_i,end_j)
+		until @closedList.include?([end_i, end_j]) || @openList.empty?
 			count = 0
 			count1 = 0
 			
@@ -55,7 +55,7 @@ class Astar
 				if (child_i >=0 && child_j >=0) && (child_i < @height && child_j <@width)
 					if (child_i != current_i || child_j != current_j) 	
 						if !@openList.include?([child_i, child_j]) && !@closedList.include?([child_i, child_j]) && @array[child_i][child_j] != "w"
-							hash= calc_G_H_F(child_i,child_j, current_i, current_j, e_i, e_j)
+							hash= calc_G_H_F(child_i,child_j, current_i, current_j, end_i, end_j)
 							@openList[[child_i, child_j]] = hash
 						end
 					end
@@ -90,7 +90,7 @@ class Astar
 			@openList.delete([current_i,current_j])
 			key = @openList.min_by{|k,v| v}
 			key = key[0]
-			extractList(key[0], key[1], e_i, e_j)
+			extract_list(key[0], key[1], end_i, end_j)
 #			
 		end
 	end
@@ -99,32 +99,32 @@ class Astar
 
 
 
-	def calc_G_H_F(c_i,c_j, p_i,p_j, e_i,e_j)
+	def calc_G_H_F(child_i,child_j, parent_i,parent_j, end_i,end_j)
 		
-		value = @openList[[p_i,p_j]]
-		if (c_i-p_i).abs == 1 && (c_j-p_j).abs == 1		
+		value = @openList[[parent_i,parent_j]]
+		if (child_i-parent_i).abs == 1 && (child_j-parent_j).abs == 1		
 			g=value[0]+14
 		else
 			g=value[0]+10
 		end			
-		h = (c_i-e_i).abs*10 + (c_j-e_j).abs*10
+		h = (child_i-end_i).abs*10 + (child_j-end_j).abs*10
 		f=h+g
-		hash= [f, g, h, p_i, p_j]	
+		hash= [f, g, h, parent_i, parent_j]	
 	end
-	$list=[]
-	def extractPath(cell_i, cell_j, s_i, s_j, list) #DONT use instance variable $list
-		until cell_i == s_i && cell_j == s_j
+
+	def extract_path(cell_i, cell_j, start_i, start_j, list) #DONT use instance variable $list
+		until cell_i == start_i && cell_j == start_j
 			parent_values= @closedList[[cell_i, cell_j]]
 			cell_i = parent_values[3]
 			cell_j = parent_values[4]	
 			list << [cell_i, cell_j]
 			
-			extractPath(cell_i, cell_j, s_i, s_j)
-			cell_i = s_i
-			cell_j = s_j
+			extract_path(cell_i, cell_j, start_i, start_j, list)
+			cell_i = start_i
+			cell_j = start_j
 		end
 		list #what happens if Ruby is pass by value, what about pass by reference
-		#puts "list after call is : #{$list} "
+		
 	end
 	
 end
@@ -132,8 +132,8 @@ end
 astar = Astar.new(15,15)
 
 wall = [[5,5],[5,6], [7,8], [8,8], [14,3], [14,4],[14,5]]
-st = [1,3]
-ed = [12,13]
-astar.findPath(st, ed, wall)
+index_of_start = [1,3]
+index_of_end = [12,13]
+astar.find_path(index_of_start, index_of_end, wall)
 
 
